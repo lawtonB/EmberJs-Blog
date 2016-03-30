447f6ef1f906b47b3f80b6a370ea8552f7e02a3b
erik tolentino and lawton browning <lawton.a.browning@gmail.com>
2016-03-30 13:52:44 -0700

    Display links on homepage including RSVP

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('blog');
-  },
+    return Ember.RSVP.hash({
+      blogs: this.store.findAll('blog')
+    });
+  }
 });
